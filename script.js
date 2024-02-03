93f9a96a005a711a2fdf3b96d04ae4211a3151e9
cutecomkid <158544484+cutecomkid@users.noreply.github.com>
2024-02-02 20:06:20 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function checkPassword() {
+  var enteredpassword = document.getElementById("password").value;
+  // Check if the entered password is correct
+  if (enteredpassword === "iVeL3731$-1!") {
+    //redirect to main page
+    window.location.href = "home.html";
+  } else {
+    alert("ViLe1337!1-$ ;)");
+  }
+}
